8e7061a2ba5d26d78794274262ebd2868ba8edac
VolkovMe <ghostfeeling@hotmail.com>
2016-05-20 14:42:12 +0600

    Start Tests

---
+++ VolkovCalc/VolkovCalc.Tests/TwoArguments/AddTest.cs
@@ -0,0 +1,17 @@
+﻿namespace VolkovCalc.Tests.OneArgument
+{
+    public class AddTest
+    {
+        [TestFixture]
+        public class ИмяТестируемогоКлассаTests
+        {
+            [Test]
+            public void ИмяТестируемогоМетодаИлиСитуацииTest()
+            {
+                // подготовительные действия
+                Assert.AreEqual(предполагаемыйРезультат, фактическийРезультат);
+            }
+            // ...
+        } 
+    }
+}
