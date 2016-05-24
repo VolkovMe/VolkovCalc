186ab824b512984ac88cf40b4029b03b047075bc
Volkov <ceverniyvolk@mail.ru>
2016-05-24 19:20:18 +0600

    New Test

---
+++ VolkovCalc/VolkovCalc.Tests/SqrTests.cs
@@ -0,0 +1,20 @@
+﻿
+namespace VolkovCalc.Tests.OneArgument
+using NUnit.Framework;
+{
+    public class SqrTests
+    {
+        [TestFixture]
+        public class SqrTests
+        {
+        [Test]           
+
+            public void CalculateTest(double argument, double result)
+            {
+                // подготовительные действия
+                Assert.AreEqual(предполагаемыйРезультат, фактическийРезультат);
+            }
+            // ...
+        }
+    }
+}
